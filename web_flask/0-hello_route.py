d0cd9c4c024591ed0228583ad7d4a0922f823c78
xander254 <alexkinyanjui356@gmail.com>
2025-02-14 03:22:54 +0300

    pep 8 validation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,12 +4,15 @@ A module that runs a flask route
 """
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """A method to return hello from hbnb"""
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)  
+    app.run(host="0.0.0.0", port=5000)
