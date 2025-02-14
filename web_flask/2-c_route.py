371b0c085caf2d46925545f610e933c8fa407811
xander254 <alexkinyanjui356@gmail.com>
2025-02-14 04:22:48 +0300

    Add: Char c change...replace underscores with spaces in /c/<text> route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,7 +23,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def cisfun(text):
     """A route that uses variables"""
-    return "C " + text.replace("_", " ") + "\n"
+    return f"C {text.replace("_", " ")}
 
 
 if __name__ == "__main__":
