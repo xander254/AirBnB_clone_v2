cf494bc71e97c9f3b2ae3080ad08a6d4f4a37e93
xander254 <alexkinyanjui356@gmail.com>
2025-02-14 03:20:10 +0300

    Run flask script

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+"""
+A module that runs a flask route
+"""
 from flask import Flask
 
-if __name__ == "__main__":
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route("/", strict_slashes=False)
-    def hello_hbnb():
-        return "<p>Hello HBNB!</p>"
-    app.run(host="0.0.0.0", port=5000, debug=True)  
-    print("Starting Flask app...")
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """A method to return hello from hbnb"""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  
